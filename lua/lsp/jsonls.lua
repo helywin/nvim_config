22a5a23e1a2282ae33a710f77f898fab22caabf7
helywin <jiang770882022@hotmail.com>
2022-05-31 18:12:05 +0800

    ini not parse with jsonls

--- lua/lsp/jsonls.lua
+++ lua/lsp/jsonls.lua
@@ -3,5 +3,7 @@ capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 return {
   capabilities = capabilities,
+  -- NOTE: let it not parse ini file!
+  tfiletypes = {"json"},
 }
 
